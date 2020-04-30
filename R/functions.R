8f85cf9605fed336f7a0754a66657e1f2457b1f3
Matthew Kling <mattkling@gmail.com>
2020-04-30 10:13:43 -0700

    update base url with new server address

--- R/functions.R
+++ R/functions.R
@@ -20,35 +20,30 @@
 #' @return a data frame of metadata for all factorial combinations of the
 #'   requested variables
 ch_queries <- function(variables, layers, models=NA, scenarios=NA, timeframes){
-      require(dplyr)
-      expand.grid(model=models,
-                  scenario=scenarios,
-                  timeframe=timeframes,
-                  variable=variables,
-                  layer=layers) %>%
-            mutate(variable2=case_when(variable=="tmin" ~ "tasmin",
-                                       variable=="tmax" ~ "tasmax",
-                                       variable=="temp" ~ "tas",
-                                       variable=="prec" ~ "pr",
-                                       variable=="bio" ~ "bio"),
-                   addendum=case_when(variable=="prec" ~ "",
-                                      TRUE ~ "_V1.2"),
-                   histdir=case_when(variable=="bio" ~ "bioclim/integer/",
-                                     variable=="prec" ~ "climatologies/prec/",
-                                     TRUE ~ paste0("climatologies/temp/integer/", variable, "/")),
-                   file=case_when(timeframe=="1979-2013" &
-                                        variable != "bio" ~ paste0(histdir, "CHELSA_",
-                                                                   variable, "10_",
-                                                                   str_pad(layer, 2, "left", "0"), "_land.7z"),
-                                  timeframe=="1979-2013" &
-                                        variable == "bio" ~ paste0(histdir, "CHELSA_",
-                                                                   variable, "10_",
-                                                                   str_pad(layer, 2, "left", "0"), ".tif"),
-                                  TRUE ~ paste0("cmip5/", timeframe, "/", variable,
-                                                "/CHELSA_", variable2, "_mon_", model,
-                                                "_", scenario, "_r*i1p1_g025.nc_", layer, "_",
-                                                timeframe, addendum, ".tif")),
-                   url=paste0("https://www.wsl.ch/lud/chelsa/data/", file))
+   require(dplyr)
+   base_url <- "https://envidatrepo.wsl.ch/uploads/chelsa/chelsa_V1/"
+   expand.grid(model = models, scenario = scenarios, timeframe = timeframes,
+               variable = variables, layer = layers) %>%
+      mutate(variable2 = case_when(variable == "tmin" ~ "tasmin",
+                                   variable == "tmax" ~ "tasmax",
+                                   variable ==
+                                      "temp" ~ "tas",
+                                   variable == "prec" ~ "pr",
+                                   variable ==
+                                      "bio" ~ "bio"),
+             addendum = case_when(variable == "prec" ~ "",
+                                  TRUE ~ "_V1.2"),
+             histdir = case_when(variable == "bio" ~ "bioclim/integer/",
+                                 variable == "prec" ~ "climatologies/prec/",
+                                 TRUE ~ paste0("climatologies/temp/integer/", variable, "/")),
+             file = case_when(timeframe == "1979-2013" &  variable != "bio" ~
+                                 paste0(histdir, "CHELSA_", variable, "10_", str_pad(layer, 2, "left", "0"), "_land.7z"),
+                              timeframe == "1979-2013" & variable == "bio" ~
+                                 paste0(histdir, "CHELSA_", variable, "10_", str_pad(layer, 2, "left", "0"), ".tif"),
+                              TRUE ~ paste0("cmip5/", timeframe, "/", variable, "/CHELSA_",
+                                            variable2, "_mon_", model, "_", scenario, "_r*i1p1_g025.nc_",
+                                            layer, "_", timeframe, addendum, ".tif")),
+             url = paste0(base_url, file))
 }
 
 #' Download CHELSA data
@@ -69,55 +64,55 @@ ch_queries <- function(variables, layers, models=NA, scenarios=NA, timeframes){
 #'   object, or any spatial object with an extent).
 ch_dl <- function(md, dest=NULL, skip_existing=TRUE, method="curl", crop=NULL){
 
-      if(is.null(dest)) dest <- getwd()
+   if(is.null(dest)) dest <- getwd()
 
-      for(i in 1:nrow(md)){
-            message(paste("File", i, "of", nrow(md), "..."))
-            md$status[i] <- "incomplete"
-            md$path[i] <- paste0(dest, "/", basename(md$file[i]))
+   for(i in 1:nrow(md)){
+      message(paste("File", i, "of", nrow(md), "..."))
+      md$status[i] <- "incomplete"
+      md$path[i] <- paste0(dest, "/", basename(md$file[i]))
 
-            runs <- c("1", "2", "12")
+      runs <- c("1", "2", "12")
 
-            if(skip_existing){
-                  # previously-failed downloads have small file size
-                  paths <- sapply(runs, function(x) sub("\\*", x, md$path[i]))
-                  size <- file.size(paths)
-                  if(any(!is.na(size) & log(size)>10)){
-                        md$path[i] <- paths[!is.na(size) & log(size)>10]
-                        md$status[i] <- "already done"
-                        next()
-                  }
-            }
-
-            # run numbers vary by model. try all options.
-            for(run in runs){
-                  url <- sub("\\*", run, md$url[i])
-                  path <- sub("\\*", run, md$path[i])
-                  r <- try(download.file(url, path, method=method, quiet=T))
-                  size <- file.size(path)
-                  if(!is.na(size) & log(size)>10){
-                        md$url[i] <- url
-                        md$path[i] <- path
-                        break()
-                  }
-                  file.remove(path)
-            }
-
-            if(class(r)=="try-error"){
-                  md$status[i] <- as.character(r)
-                  next()
-            }
-            if(file.exists(md$path[i])) md$status[i] <- "download completed"
-
-            if(!is.null(crop) & file.exists(md$path[i])){
-                  require(raster)
-                  r <- raster(md$path[i]) %>%
-                        crop(crop) %>%
-                        writeRaster(md$path[i], overwrite=T)
-                  md$status[i] <- "raster cropped"
-            }
+      if(skip_existing){
+         # previously-failed downloads have small file size
+         paths <- sapply(runs, function(x) sub("\\*", x, md$path[i]))
+         size <- file.size(paths)
+         if(any(!is.na(size) & log(size)>10)){
+            md$path[i] <- paths[!is.na(size) & log(size)>10]
+            md$status[i] <- "already done"
+            next()
+         }
       }
-      return(md)
+
+      # run numbers vary by model. try all options.
+      for(run in runs){
+         url <- sub("\\*", run, md$url[i])
+         path <- sub("\\*", run, md$path[i])
+         r <- try(download.file(url, path, method=method, quiet=T))
+         size <- file.size(path)
+         if(!is.na(size) & log(size)>10){
+            md$url[i] <- url
+            md$path[i] <- path
+            break()
+         }
+         file.remove(path)
+      }
+
+      if(class(r)=="try-error"){
+         md$status[i] <- as.character(r)
+         next()
+      }
+      if(file.exists(md$path[i])) md$status[i] <- "download completed"
+
+      if(!is.null(crop) & file.exists(md$path[i])){
+         require(raster)
+         r <- raster(md$path[i]) %>%
+            crop(crop) %>%
+            writeRaster(md$path[i], overwrite=T)
+         md$status[i] <- "raster cropped"
+      }
+   }
+   return(md)
 }
 
 
